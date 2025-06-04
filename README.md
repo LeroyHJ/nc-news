e3267dedb105c930c586d2dbaafa816f4bd51b2b
LeroyHJ <leroy_jallow_@hotmail.co.uk>
2025-06-04 15:38:09 +0100

    seeding complete

--- README.md
+++ README.md
@@ -1,3 +1,49 @@
 # NC News Seeding
 
-- Instructions for this sprint can be found at https://l2c.northcoders.com/courses/be/seeding-nc-news
+Creating the databases
+This project requires two databases:
+
+A development database (for realistic data).
+A test database (with simpler test data).
+
+Step 1: Run the setup script
+To create both databases, run:
+
+npm run setup-dbs
+This will run the setup-dbs.sql file to create both a test and development database.
+
+Step 2: Set up environment variables
+You must create two .env files for your databases:
+
+.env.test (for the test database).
+.env.development (for the development database).
+Double-check that your .gitignore file includes .env.* so these files aren't pushed to GitHub.
+
+Step 3: Verify your setup
+Run the following scripts and check the console logs (you may need to scroll up a bit in the terminal!):
+
+npm run test-seed
+This runs tests for your seed function.
+No tests will pass yet (which is expected), but you should see logs confirming you are connected to your test database.
+npm run seed-dev
+This runs the run-seed script, which calls your seed function with development data.
+You will see errors at this stage, but the logs should confirm that you are connected to the development database.
+Understanding the Project Structure
+The db folder contains everything you need to start creating the seed function:
+
+A data folder
+This contains subfolders with the test and development data.
+Each of these contains an index.js file which acts as a single entry point for all data in the folder.
+When you need access to the data elsewhere, you can write one convenient require statement to the index file, rather than having to require each file individually.
+Think of it like the index of a book - a place to refer to!
+Make sure the index file exports an object with values of the data from that folder with the keys:
+articleData
+commentData
+topicData
+userData
+
+A seeds folder
+This contains the scripts for seeding your databases.
+Any utility functions we need for data-manipulation are also found here.
+
+Also dont forget to run "npm install" to install all the packages and dependencies for husky and tests/jest files to work locally on your machine.
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,125 @@
-const db = require("../connection")
+const db = require("../connection");
+const format = require("pg-format");
+const {
+  convertTimestampToDate,
+  createArticleRef,
+  formatComments,
+} = require("./utils");
 
-const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(); //<< write your first query in here.
+const seed = ({ topicData,
+  userData,
+  articleData,
+  commentData }) => {
+  return db
+    .query("DROP TABLE IF EXISTS comments;")
+    .then(() => db.query("DROP TABLE IF EXISTS articles;"))
+    .then(() => db.query("DROP TABLE IF EXISTS users;"))
+    .then(() => db.query("DROP TABLE IF EXISTS topics;"))
+    .then(() => {
+      return db.query(`CREATE TABLE topics(
+        slug VARCHAR(20) PRIMARY KEY,
+        description VARCHAR(1000) NOT NULL,
+        img_url VARCHAR(1000)
+        );
+      `);
+    })
+    .then(() => {
+      return db.query(`
+        CREATE TABLE users (
+          username VARCHAR(20) PRIMARY KEY,
+          name VARCHAR(20) NOT NULL,
+          avatar_url VARCHAR(1000) NOT NULL
+        );
+      `);
+    })
+    .then(() => {
+      return db.query(`
+        CREATE TABLE articles (
+          article_id SERIAL PRIMARY KEY,
+          title VARCHAR(250) NOT NULL,
+          topic VARCHAR(20) REFERENCES topics(slug),
+          author VARCHAR(20) REFERENCES users(username),
+          body TEXT NOT NULL,
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+          votes INT DEFAULT 0,
+          article_img_url VARCHAR(1000)
+        );
+      `);
+    })
+    .then(() => {
+      return db.query(`
+        CREATE TABLE comments (
+          comment_id SERIAL PRIMARY KEY,
+          article_id INT REFERENCES articles(article_id),
+          body TEXT,
+          votes INTEGER DEFAULT 0,
+          author VARCHAR(50) REFERENCES users(username),
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+      `);
+    })
+
+    // topics
+    .then(() => {
+      const insertTopicsQueryStr = format(
+        "INSERT INTO topics (slug, description) VALUES %L RETURNING *;",
+        topicData.map(({ slug, description }) => [slug, description])
+      );
+      return db.query(insertTopicsQueryStr);
+    })
+    //users
+    .then(() => {
+      const insertUsersQueryStr = format(
+        "INSERT INTO users (username, name, avatar_url) VALUES %L RETURNING *;",
+        userData.map(({ username, name, avatar_url }) => [
+          username,
+          name,
+          avatar_url,
+        ])
+      );
+      return db.query(insertUsersQueryStr);
+    })
+    //articles
+    .then(() => {
+      const formattedArticles = articleData.map(convertTimestampToDate);
+      const insertArticlesQueryStr = format(
+        `INSERT INTO articles (title, topic, author, body, created_at, votes)
+         VALUES %L RETURNING *;`,
+        formattedArticles.map(
+          ({ title, topic, author, body, created_at, votes = 0 }) => [
+            title,
+            topic,
+            author,
+            body,
+            created_at,
+            votes,
+          ]
+        )
+      );
+      return db.query(insertArticlesQueryStr);
+    })
+    .then((result) => {
+      const articleRows = result.rows;
+      const articleRef = createArticleRef(articleRows);
+
+      //comments
+      const insertCommentsQueryStr = format(
+        `INSERT INTO comments (author, article_id, votes, created_at, body)
+         VALUES %L RETURNING *;`,
+        commentData.map(
+          ({ author, article_title, votes = 0, created_at, body }) => {
+            return [
+            author,
+            articleRef[article_title],
+            votes,
+            new Date(created_at),
+            body,
+          ]
+        }
+        )
+      );
+      return db.query(insertCommentsQueryStr);
+    });
 };
+
 module.exports = seed;
