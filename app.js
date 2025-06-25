4786292255fd7a4c6b43de3ae65c3d12e4a2bd82
LeroyHJ <leroy_jallow_@hotmail.co.uk>
2025-06-25 10:42:54 +0100

    added env production

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('cors');
 const { getApi } = require("./controllers/api.controller");
 const { getTopics } = require("./controllers/topics.controller");
 const {
@@ -20,6 +21,9 @@ const {
 
 const app = express();
 //aaa
+
+app.use(cors());
+
 app.use(express.json());
 
 app.get("/api", getApi);
